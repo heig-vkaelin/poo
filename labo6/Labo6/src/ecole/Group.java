e71218fcd95c906a66c4f7211f01db62e4f08f5f
Valentin Kaelin <kalanehd@gmail.com>
2021-11-04 15:55:36 +0100

    feat: start main

--- labo6/Labo6/src/ecole/Group.java
+++ labo6/Labo6/src/ecole/Group.java
@@ -4,11 +4,14 @@ public class Group {
     private final int number;
     private final String orientation;
     private final int trimester;
+    private final Student[] students;
     
-    public Group(int number, String orientation, int trimester) {
+    public Group(int number, String orientation, int trimester, Student[] students) {
         this.number = number;
         this.orientation = orientation;
         this.trimester = trimester;
+        // TODO: deep copy
+        this.students = students;
     }
     
     public String schedule() {
@@ -21,7 +24,7 @@ public class Group {
     }
     
     public int nbStudents() {
-        return -42;
+        return students.length;
     }
     
     public void defineLessons(String... args) {
--- labo6/Labo6/src/ecole/Main.java
+++ labo6/Labo6/src/ecole/Main.java
@@ -3,20 +3,47 @@ package ecole;
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("Hello World from Labo6!");
-        
-        // Définir les trois leçons du cours de POO (PDO), la leçon
-        // du cours SYE (DRE) ainsi qu’une leçon TIC (projet non encadré).
-        
-        // Professeurs
+        // 1. Professeurs
         Teacher pdo = new Teacher("Pier", "Donini", "PDO");
         Teacher dre = new Teacher("Daniel", "Rossier", "DRE");
         
-        // Leçons
+        // 2. Leçons
         Lesson poo1 = new Lesson("POO", 3, 6, 2, "H02", pdo);
         Lesson poo2 = new Lesson("POO", 4, 6, 2, "H02", pdo);
         Lesson poo3 = new Lesson("POO", 4, 8, 2, "H02", pdo);
-        
         Lesson sye = new Lesson("SYE", 1, 1, 2, "G01", dre);
+        Lesson tic = new Lesson("SYE", 2, 10, 1, "F06");
+        
+        // 3. Élèves
+        Student john = new Student("John", "Lennon", 1234);
+        Student paul = new Student("Paul", "Mc Cartney", 2341);
+        Student ringo = new Student("Ringo", "Starr", 3241);
+        Student george = new Student("George", "Harisson", 4321);
+        Student roger = new Student("Roger", "Waters", 1324);
+        Student david = new Student("David", "Gilmour", 4312);
+        
+        // 4. Groupes
+        Group il6 = new Group(1, "IL", 6, new Student[]{john, paul, ringo, george});
+        Group si6 = new Group(1, "SI", 6, new Student[]{roger, david});
+        
+        // 5. TODO
+        
+        // 6. Personnes
+        Person[] people = new Person[]{
+                pdo, dre, john, paul, ringo, george, roger, david
+        };
+        for (Person p : people) {
+            System.out.println(p);
+        }
+        
+        
+        
+        /*
+        5. Affecter au groupe IL6-1 toutes les leçons existantes. Affecter au
+        groupe SI6-1 toutes les leçons de POO.
+        7. Afficher les informations relatives au groupe IL6-1 (nom, nombre
+        d’étudiants, horaire).
+        8. Afficher l’horaire du professeur PDO.
+         */
     }
 }
