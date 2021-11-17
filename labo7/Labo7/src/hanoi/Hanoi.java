47076183f844c4955db306e3dd53f941c316e79c
Jonathan <chil.tfm@gmail.com>
2021-11-17 14:26:10 +0100

    feat: check if the arg given by the user is correct

--- labo7/Labo7/src/hanoi/Hanoi.java
+++ labo7/Labo7/src/hanoi/Hanoi.java
@@ -1,10 +1,35 @@
 package hanoi;
 
 public class Hanoi {
-    public static void main(String[] args) {
+    public static void main(String[] args)throws RuntimeException {
+        int numberOfDisk = testArgs(args);
+
         System.out.println("Hello World from Labo7!");
     }
-    
+
+    /**
+     * Fonction permettant de tester que le paramètre passé par l'utilisateur est correct (un entier > 0).
+     * @param args est le tableau d'argument passé par l'utilisateur.
+     * @return La valeur passée par l'utilisateur, castée en int.
+     * @throws RuntimeException
+     */
+    private static int testArgs(String[] args)throws RuntimeException {
+        int numberOfDisk;
+        if (args.length != 1) {
+            throw new RuntimeException("Il ne faut qu'un seul argument (exemple: java Hanoi 7)");
+        }
+        try {
+            numberOfDisk = Integer.parseInt(args[0]);
+        } catch (Exception e) {
+            throw new RuntimeException("L'argument doit être un entier.");
+        }
+
+        if (numberOfDisk < 1) {
+            throw new RuntimeException("L'argument doit être un entier > 1");
+        }
+        return numberOfDisk;
+    }
+
     /**
      * Constructeur générique
      *
@@ -12,27 +37,27 @@ public class Hanoi {
      * @param displayer
      */
     public Hanoi(int disks, HanoiDisplayer displayer) {
-    
+
     }
-    
+
     /**
      * Constructeur pour l'affichage de la console
      *
      * @param disks
      */
     public Hanoi(int disks) {
-    
+
     }
-    
+
     /**
      * Déplace tous les disques de la première aiguille à la troisième en
      * affichant les états successifs des aiguilles au moyen de l'instance
      * HanoiDisplayer sélectionnée.
      */
     public void solve() {
-    
+
     }
-    
+
     /**
      * Rend un tableau de tableaux représentant l'état des aiguilles. Pour un tel
      * tableau t, l'élément t[i][j] correspond à la taille du j-ème disque (en
@@ -43,7 +68,7 @@ public class Hanoi {
     public int[][] status() {
         return null;
     }
-    
+
     /**
      * Permet de vérifier si la solution est atteinte
      *
@@ -52,7 +77,7 @@ public class Hanoi {
     public boolean finished() {
         return false;
     }
-    
+
     /**
      * Rend le nombre de disques déplacés
      *
