1e8a1a2e3e663e788e39106fcad5db18b19313f2
Jonathan <chil.tfm@gmail.com>
2021-11-25 14:33:36 +0100

    docs: comment and throw exception

--- labo7/Labo7/src/Hanoi.java
+++ labo7/Labo7/src/Hanoi.java
@@ -5,9 +5,16 @@ import hanoi.gui.JHanoi;
  */
 public class Hanoi {
     /**
-     * @param args
+     * Méthode appelée au lancement du programme, teste le nombre de paramètre et la validité de ce dernier.
+     * Si paramètre valide, résouds la tour de Hanoï avec le nombre de disque voulu.
+     * @param args 0 ou 1:
+     *             Si 0 paramètre, utilise l'interface graphique.
+     *             Si 1 paramètre, utilise l'interface console. Le paramètre représente le nombre de disque.
+     *             Il doit être positif.
+     *             Si 2 paramètre ou plus, le programme lance une RuntimeException.
+     * @throws Exception en cas d'entrée utilisateur incorrecte.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         if (args.length == 0) {
             new JHanoi();
         } else {
@@ -21,23 +28,23 @@ public class Hanoi {
      *
      * @param args est le tableau d'argument passé par l'utilisateur.
      * @return La valeur passée par l'utilisateur, castée en int.
-     * @throws RuntimeException
+     * @throws Exception
      */
-    private static int testArgs(String[] args) {
+    private static int testArgs(String[] args) throws Exception {
         int numberOfDisk;
         // Si il y a 0 argument passé par l'utilisateur, on utilise l'interface graphique.
         if (args.length > 1)
-            throw new RuntimeException("Il ne faut qu'un seul argument (exemple: java Hanoi 7)");
+            throw new Exception("Il ne faut qu'un seul argument (exemple: java Hanoi 7)");
         
         try {
             numberOfDisk = Integer.parseInt(args[0]);
             
         } catch (Exception e) {
-            throw new RuntimeException("L'argument doit être un entier positif.");
+            throw new Exception("L'argument doit être un entier positif.");
         }
         
         if (numberOfDisk < 0)
-            throw new RuntimeException("L'argument doit être un entier positif");
+            throw new Exception("L'argument doit être un entier positif");
         
         return numberOfDisk;
     }
