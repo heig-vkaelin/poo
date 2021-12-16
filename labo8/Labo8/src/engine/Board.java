dbd187985079e09c97e9c0cf9e30c3b547b2b252
Jonathan <chil.tfm@gmail.com>
2021-12-16 16:06:00 +0100

    refactor: upgrade the method which put pieces at start

--- labo8/Labo8/src/engine/Board.java
+++ labo8/Labo8/src/engine/Board.java
@@ -16,8 +16,25 @@ public class Board {
     
     private void setStartingPositions() {
         pieces = new Piece[BOARD_SIZE][BOARD_SIZE];
-        
-        // Queens
+        PlayerColor color = PlayerColor.WHITE;
+
+        for(int line = 0; line < 9; line += 7, color = PlayerColor.BLACK){
+            // Reine
+            pieces[3][line] = new Queen(new Cell(3, line), color);
+            // Roi
+            pieces[4][line] = new King(new Cell(4, line), color);
+            // Tours, cavaliers et fous
+            for (int i = 0; i < 2; i++) {
+                pieces[i * 7][line] = new Rook(new Cell(i * 7, line), color);
+                pieces[1 + i * 5][line] = new Knight(new Cell(1 + i * 5, line), color);
+                pieces[2 + i * 3][line] = new Bishop(new Cell(2 + i * 3, line), color);
+            }
+            // Pions
+            for (int i = 0; i < 8; i++) {
+                pieces[i][(line * 5 / 7) + 1] = new Pawn(new Cell(i, (line * 5 / 7) + 1), color);
+            }
+        }
+        /*// Queens
         pieces[3][0] = new Queen(new Cell(3, 0), PlayerColor.WHITE);
         pieces[3][7] = new Queen(new Cell(3, 7), PlayerColor.BLACK);
         
@@ -41,7 +58,7 @@ public class Board {
         for (int i = 0; i < 8; i++) {
             pieces[i][1] = new Pawn(new Cell(i, 1), PlayerColor.WHITE);
             pieces[i][6] = new Pawn(new Cell(i, 6), PlayerColor.BLACK);
-        }
+        }*/
     }
     
     public Piece getPiece(int x, int y) {
