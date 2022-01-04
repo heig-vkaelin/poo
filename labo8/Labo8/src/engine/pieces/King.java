868e0c249fe1a2a6d8acc672cd83df92bd36e181
Valentin Kaelin <kalanehd@gmail.com>
2022-01-04 14:49:22 +0100

    fix: castle should work

--- labo8/Labo8/src/engine/pieces/King.java
+++ labo8/Labo8/src/engine/pieces/King.java
@@ -31,41 +31,38 @@ public class King extends FirstMoveSpecificPiece {
     }
     
     private boolean castle(Cell to) {
-        // TODO: check castle
-        // Si le roi à bougé ou qu'il est en échec, il ne peut plus rock
-        if(hasMoved()/* || getBoard().isCheck(getColor())*/) { return false; } // si on met ça y'a un crash
-        // Exception in thread "AWT-EventQueue-0" java.lang.StackOverflowError
-        // Si le roi va pas deux cases à gauche ou à droite, c'est faux
-        int deltaY = to.getY() - getCell().getY();
-        int deltaX = to.getX() - getCell().getX();
-        if(Math.abs(deltaX) != 2 || Math.abs(deltaY) != 0){
-            return false;
-        }
-        // A droite
-        if(deltaX > 0){
-            for(int i = 1; i < 3; i++){
-                // Si la case n'est pas vide
-                if(getBoard().getPiece(new Cell(getCell().getX() + i,getCell().getY())) != null){ return false; }
-                // Si la case est attaquée
-                if(getBoard().isAttacked(getColor(), new Cell(getCell().getX() + i,getCell().getY()))){ return false; }
-            }
-            return true;
-        } else { // A gauche
-            for(int i = 1; i < 3; i++){
-                // Si la case n'est pas vide
-                if(getBoard().getPiece(new Cell(getCell().getX() - i,getCell().getY())) != null){ return false; }
-                // Si la case est attaquée
-                if(getBoard().isAttacked(getColor(), new Cell(getCell().getX() - i,getCell().getY()))){ return false; }
-            }
-            // on doit uniquement controler que cette case est vide, vu que le roi ne passe pas dessus.
-            if(getBoard().getPiece(new Cell(getCell().getX() - 3,getCell().getY())) != null){ return false; }
-            return true;
-        }
-        /*
+       /*
             Le petit et le grand roque doivent être fonctionnels. Leur mouvement est initié en bougeant le roi de deux
             cases vers la droite ou vers la gauche. Ce coup ne peut être effectué si le roi est en échec, s’il a déjà bougé,
             si la tour concernée a déjà bougé ou si une des cases sur lesquelles le roi passe est en échec.
         */
+        
+        int deltaY = to.getY() - getCell().getY();
+        int deltaX = to.getX() - getCell().getX();
+        if (hasMoved() || Math.abs(deltaX) != 2 || deltaY != 0)
+            return false;
+        
+        boolean leftSide = deltaX < 0;
+        int sign = leftSide ? -1 : 1;
+        Cell direction = new Cell(sign, 0);
+        Cell rookCell = new Cell(leftSide ? 0 : Board.BOARD_SIZE - 1, getCell().getY());
+        Piece rook = getBoard().getPiece(rookCell);
+        Cell rookDestination = getCell().add(direction);
+        
+        if (!(rook instanceof Rook) || ((Rook) rook).hasMoved() || !rook.checkMove(rookDestination))
+            return false;
+        
+        for (int i = 0; i < 3; i++) {
+            Cell position = getCell().add(direction.multiply(i * sign));
+            System.out.println("x: " + position.getX() + " y: " + position.getY());
+            if (getBoard().isAttacked(getColor(), position))
+                return false;
+        }
+        
+        getBoard().applyMove(rook, rookDestination);
+        rook.postUpdate();
+        
+        return true;
     }
 
 //    @Override
