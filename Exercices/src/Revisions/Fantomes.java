1dd7573d96101a28049b414e057fead6844e0dd6
Valentin Kaelin <kalanehd@gmail.com>
2022-01-18 20:50:55 +0100

    feat: add fantomes exercice

--- Exercices/src/Revisions/Fantomes.java
+++ Exercices/src/Revisions/Fantomes.java
@@ -1,9 +1,12 @@
 package Revisions;
 
+import java.util.LinkedList;
+import java.util.List;
+
 enum Lieu {Bree, Rivendell, Lorien, Moria};
 
 abstract class Avatar {
-    private String nom;
+    private final String nom;
     private Lieu position;
     
     public Avatar(String nom, Lieu position) {
@@ -31,8 +34,8 @@ abstract class Avatar {
 }
 
 class Equipement {
-    private String nom;
-    private int niveau;
+    private final String nom;
+    private final int niveau;
     
     public Equipement(String nom, int niveau) {
         this.nom = nom;
@@ -44,42 +47,96 @@ class Equipement {
     }
 }
 
-class Personnage {
-
+class Personnage extends Avatar {
+    private final int niveau;
+    private final List<Equipement> equipements;
+    private Avatar fantome;
+    
+    public Personnage(String nom, Lieu position, int niveau) {
+        super(nom, position);
+        this.niveau = niveau;
+        equipements = new LinkedList<>();
+    }
+    
+    public void equiper(Equipement equipement) {
+        equipements.add(equipement);
+    }
+    
+    public Avatar mourir() {
+        if (fantome != null)
+            return fantome;
+        
+        System.out.println(nom() + " meurt!");
+        return fantome = new Avatar("Le fantôme de " + nom(), Lieu.Lorien) {
+            @Override
+            public void deplacer(Lieu position) {
+                super.deplacer(position);
+                if (position == Personnage.this.position()) {
+                    System.out.println(nom() + " retrouve son corps et le ressucite!");
+                    ressusciter();
+                }
+            }
+        };
+    }
+    
+    public Avatar fantome() {
+        return fantome;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(super.toString());
+        sb.append(", niveau: ").append(niveau).append(",");
+        for (Equipement e : equipements)
+            sb.append(" ").append(e.nom());
+        return sb.toString();
+    }
+    
+    private void ressusciter() {
+        fantome = null;
+    }
+    
+    @Override
+    public void deplacer(Lieu position) {
+        if (fantome == null)
+            super.deplacer(position);
+        else
+            System.out.println(nom() + " est mort et ne peut pas se déplacer...");
+    }
 }
 
 public class Fantomes {
     public static void main(String[] args) {
         System.out.println("- 0 -");
         Personnage gandalf = new Personnage("Gandalf", Lieu.Bree, 50);
-    
+        
         gandalf.equiper(new Equipement("Epée", 20));
         gandalf.equiper(new Equipement("Baton", 40));
-    
+        
         System.out.println("- 1 -");
         System.out.println(gandalf);
-    
+        
         System.out.println("- 2 -");
-        System.out.println(Lieu.Moria);
-    
+        gandalf.deplacer(Lieu.Moria);
+        
         System.out.println("- 3 -");
         Avatar fantome = gandalf.mourir();
-    
+        
         System.out.println("- 4 -");
         System.out.println(fantome == gandalf.fantome());
-    
+        
         System.out.println("- 5 -");
         gandalf.deplacer(Lieu.Rivendell);
-    
+        
         System.out.println("- 6 -");
         fantome.deplacer(Lieu.Rivendell);
-    
+        
         System.out.println("- 7 -");
         fantome.deplacer(Lieu.Moria);
-    
+        
         System.out.println("- 8 -");
         System.out.println(gandalf.fantome() != null);
-    
+        
         System.out.println("- 9 -");
         gandalf.deplacer(Lieu.Lorien);
     }
